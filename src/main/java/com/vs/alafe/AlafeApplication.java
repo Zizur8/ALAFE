1e2eb2790697ab6a06120d632331d72c5840f4f2
CesarVS <cesar8vs@gmail.com>
2025-10-14 12:45:28 -0500

    Primer commit

---
+++ src/main/java/com/vs/alafe/AlafeApplication.java
@@ -0,0 +1,13 @@
+package com.vs.alafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vs/alafe/AlafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.alafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
