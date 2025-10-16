498c3d94ce02f8535e67e57306e1a0d7b9be0f8b
Dana Garcia <dna.jg03@gmail.com>
2025-10-16 07:59:22 -0500

    mi cambio en homeController

--- src/main/java/com/vs/alafe/controller/HomeController.java
+++ src/main/java/com/vs/alafe/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 //Cambio para git dev
+//oro cambio
 @Controller
 @RequestMapping("/index")
 public class HomeController {
